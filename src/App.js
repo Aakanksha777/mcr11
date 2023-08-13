3b647fd11f226deb6b7a8f6ecc91c805ebbe4b6b
Aakanksha <109036438+Aakanksha777@users.noreply.github.com>
2023-08-13 10:39:59 +0530

    basic Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import {Routes, Route, NavLink } from 'react-router-dom';
+import Home from './components/Home';
+import Cart from './components/Cart';
 
 function App() {
   return (
     <div className="App">
       <h2>MCR-11 on progress.</h2>
+      <NavLink to='/'>Home</NavLink>
+      <NavLink to='/cart'>Cart</NavLink>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+      </Routes>
     </div>
   );
 }
